134039d5e4f1b35c50ea5bb5adf9ec5d12f8726f
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-08-01 00:57:32 -0300

    DesafiosConcluidos

--- Teste1/src/main/java/com/senac/refactor/Circle.java
+++ Teste1/src/main/java/com/senac/refactor/Circle.java
@@ -4,18 +4,44 @@ import java.util.Scanner;
 
 public class Circle {
     public static void main(String[] args) {
-        Scanner PI = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("""
                 1- Calculos de area
                 2- Calculos de perimetro
                 3- Calculos de volume
-                4- Calculos de areaasdadsad""");
+                4- Calculos de area""");
 
-        int CalculoDeArea = PI.nextInt();
-        PI.nextLine();
+        int option = scanner.nextInt();
+        scanner.nextLine();
 
+        System.out.println("Digite o valor do raio: ");
+        double raio = scanner.nextDouble();
+        scanner.nextLine();
+
+        Circle circle = new Circle();
+        switch (option) {
+            case 1:
+                double area = circle.calculaAreaDoCirculo(raio);
+                System.out.println("A área do círculo é: " + area);
+                break;
+            case 2:
+                double perimetro = circle.calculaPerimetroDoCirculo(raio);
+                System.out.println("O perímetro do círculo é: " + perimetro);
+                break;
+            case 3:
+                double volume = circle.calculaVolumeDaEsfera(raio);
+                System.out.println("O volume da esfera é: " + volume);
+                break;
+            case 4:
+                double areaSuperficie = circle.calculaAreaDaSuperficieDaEsfera(raio);
+                System.out.println("A área da superfície da esfera é: " + areaSuperficie);
+                break;
+            default:
+                System.out.println("Opção inválida.");
+        }
     }
+
     public double calculaAreaDoCirculo(double raio) {
         double area = Math.PI * raio * raio;
         return area;
