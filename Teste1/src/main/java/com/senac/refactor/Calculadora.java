fb0a8dcaab92f758bbf1dbe54ec956036a6b8e15
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-07-31 21:19:13 -0300

    DesafioBugsSenac

--- Teste1/src/main/java/com/senac/refactor/Calculadora.java
+++ Teste1/src/main/java/com/senac/refactor/Calculadora.java
@@ -12,7 +12,7 @@ public class Calculadora {
         System.out.println("2. Subtração");
         System.out.println("3. Multiplicação");
         System.out.println("4. Divisão");
-        System.out.println("5 jsdsd");
+        System.out.println("5 jsdsdasfaf");
 
         int operacao = sc.nextInt();
 
